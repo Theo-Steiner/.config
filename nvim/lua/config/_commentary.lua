ed11b5e985b32f4ce99cd53a334c30b575d2e014
Theodor Steiner <theosteiner@gmail.com>
2023-03-29 22:42:18 +0900

    switch to commentstring default keybindings

--- nvim/lua/config/_commentary.lua
+++ nvim/lua/config/_commentary.lua
@@ -1,18 +1,5 @@
 return {
+	keys = { { 'g' } },
 	"tpope/vim-commentary",
-	keys = {
-		-- <leader> c toggles the current line or current selection to be a comment
-		{
-			"<leader>c",
-			function()
-				require('ts_context_commentstring.internal').update_commentstring()
-				vim.api.nvim_feedkeys(
-					vim.api.nvim_replace_termcodes(":Commentary<CR>", true, false, true),
-					"",
-					false)
-			end,
-			mode = { "n", "v" },
-			silent = true
-		}
-	}
+	dependencies = "JoosepAlviste/nvim-ts-context-commentstring"
 }
