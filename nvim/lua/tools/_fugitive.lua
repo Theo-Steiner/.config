8508f1c13af2dd735dd1e5858f61cdd2a329e8f7
Theodor Steiner <theosteiner@gmail.com>
2023-08-03 18:58:04 +0900

    feat: add keymaps for fugitive

--- nvim/lua/tools/_fugitive.lua
+++ nvim/lua/tools/_fugitive.lua
@@ -4,9 +4,26 @@ return {
 		"G", "Git", "Gread", "Gwrite", "Gdiff"
 	},
 	keys = {
-
+		-- use <leader>gc to pretype commit
+		{
+			"<leader>gc",
+			":G commit -m \"",
+		},
+		-- use <leader>g to open fugitive
+		{
+			"<leader>gr",
+			":Gread<cr>",
+			silent = true,
+		},
+		-- use <leader>g to open fugitive
+		{
+			"<leader>gw",
+			":Gwrite<cr>",
+			silent = true,
+		},
 	},
 	init = function()
+		-- make default diff view vertical
 		vim.opt.diffopt = vim.opt.diffopt + "vertical"
 	end,
 }
