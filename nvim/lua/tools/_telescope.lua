bd10ff18782b5ff5ad2bd977b0006923c21545d2
Theodor Steiner <theosteiner@gmail.com>
2023-08-08 12:28:08 +0900

    feat: add 'fp' command to navigate projects

--- nvim/lua/tools/_telescope.lua
+++ nvim/lua/tools/_telescope.lua
@@ -13,7 +13,7 @@ end
 
 return {
 	"nvim-telescope/telescope.nvim",
-	dependencies = { "nvim-lua/plenary.nvim", "nvim-treesitter/nvim-treesitter",
+	dependencies = { "nvim-lua/plenary.nvim", "nvim-treesitter/nvim-treesitter", 'cljoly/telescope-repo.nvim',
 		{
 			"Theo-Steiner/togglescope",
 			-- uncomment to use from ~projects/togglescope
@@ -35,10 +35,16 @@ return {
 			"<leader>fg",
 			function()
 				require('telescope').extensions.togglescope.live_grep({ default_text = getVisualSelection() })
-			end
-			,
+			end,
 			silent = true,
 			mode = { "n", "v" }
+		},
+		-- find projects using cljoly/telescope-repo.nvim
+		{
+			"<leader>fp",
+			function()
+				require 'telescope'.extensions.repo.list()
+			end,
 		},
 		-- use <leader> fc to search for merge conflicts
 		{ "<leader>fc", "<cmd>Telescope grep_string search=<<<<<<<<cr>", silent = true },
@@ -59,6 +65,21 @@ return {
 								"!package-lock.json",
 							},
 							togglescope_title = "Live Grep (hidden)"
+						}
+					}
+				},
+				repo = {
+					list = {
+						search_dirs = {
+							"~/dev",
+						},
+						-- not yet implemented https://github.com/cljoly/telescope-repo.nvim/issues/56 maybe do a PR
+						mappings = {
+							i = {
+								['<CR>'] = function()
+									print('hello')
+								end
+							}
 						}
 					}
 				}
