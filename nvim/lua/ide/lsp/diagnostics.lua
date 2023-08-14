434e7f8f276f500e65a8ec176a7e0fd8533e54f1
Theodor Steiner <theosteiner@gmail.com>
2023-08-14 12:40:26 +0900

    chore: unify diagnostic sign warnings

--- nvim/lua/ide/lsp/diagnostics.lua
+++ nvim/lua/ide/lsp/diagnostics.lua
@@ -4,22 +4,25 @@
 local M = {}
 
 M.setup = function()
+	-- icons for diagnostic errors
 	vim.fn.sign_define(
-		'DiagnosticSignError',
-		{ text = '', texthl = 'DiagnosticSignError' }
+		"DiagnosticSignError",
+		{ text = " ", texthl = "DiagnosticSignError" }
 	)
 	vim.fn.sign_define(
-		'DiagnosticSignWarn',
-		{ text = '', texthl = 'DiagnosticSignWarn' }
+		"DiagnosticSignWarn",
+		{ text = " ", texthl = "DiagnosticSignWarn" }
 	)
 	vim.fn.sign_define(
-		'DiagnosticSignHint',
-		{ text = '', texthl = 'DiagnosticSignHint' }
+		"DiagnosticSignInfo",
+		{ text = " ", texthl = "DiagnosticSignInfo" }
 	)
 	vim.fn.sign_define(
-		'DiagnosticSignInfo',
-		{ text = '', texthl = 'DiagnosticSignInfo' }
+		"DiagnosticSignHint",
+		{ text = "󰌵", texthl = "DiagnosticSignHint" }
 	)
+
+	-- make diagnostics show in float
 	vim.diagnostic.config({
 		virtual_text = false,
 		float = {
