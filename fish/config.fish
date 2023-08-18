da1feb4f362159b1231042cb3f5ff5c6d44e79b1
Theodor Steiner <theosteiner@gmail.com>
2023-08-18 11:15:12 +0900

    feat: make cursor change properly in fish vi mode

--- fish/config.fish
+++ fish/config.fish
@@ -1,26 +1,33 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
-    fish_vi_key_bindings
-	for mode in insert default visual
-		bind -M $mode \cp up-or-search
-		bind -M $mode \cn down-or-search
-		bind -M $mode \cf forward-char
-	end
+  fish_vi_key_bindings
+  # allow special cursors for vi modes
+  set -g fish_vi_force_cursor 1
+  set fish_cursor_default block
+  set fish_cursor_insert line
+
+  for mode in insert default visual
+    # search up and down with <C-P> / <C-N>
+    bind -M $mode \cp up-or-search
+    bind -M $mode \cn down-or-search
+    # complete with <C-F>
+    bind -M $mode \cf forward-char
+  end
 end
 
 export EDITOR='nvim'
 export VISUAL='nvim'
 
+# alias nvim to vi
 function vi
-	nvim $argv
+  nvim $argv
 end
 
 function vic
-	cd ~/.config/nvim && vi init.lua
+  cd ~/.config/nvim && vi init.lua
 end
 
 function fish_greeting
-	echo User: (set_color blue; echo Theo-Steiner; set_color normal)
-    echo Time: (set_color yellow; date +%T; set_color normal)
-	echo Device: (set_color purple; echo $hostname; set_color normal)
+  echo User: (set_color blue; echo Theo-Steiner; set_color normal)
+  echo Time: (set_color yellow; date +%T; set_color normal)
+  echo Device: (set_color purple; echo $hostname; set_color normal)
 end
