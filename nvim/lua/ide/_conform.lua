0b82d99e7872515dcde29808e3e9c5aa53e19d4a
Theodor Steiner <theosteiner@gmail.com>
2024-02-01 10:27:48 +0900

    fix: add helper for auto async formatting

--- nvim/lua/ide/_conform.lua
+++ nvim/lua/ide/_conform.lua
@@ -6,21 +6,30 @@ return {
 		require("conform").setup({
 			formatters_by_ft = {
 				lua = { "stylua" },
-				javascript = { { "prettier" } },
+				javascript = { "prettier" },
+				typescript = { "prettier" },
+				vue = { "prettier" }
 			},
 			format_on_save = function(bufnr)
 				-- Disable sync formatting when autoformatting is disabled, or sync formatting for the filetype too slow
 				if vim.g.disable_autoformat or slow_format_filetypes[vim.bo[bufnr].filetype] then
 					return
 				end
-				return { timeout_ms = 200, lsp_fallback = true }
+
+				local function on_format(err)
+					if err and err:match("timeout$") then
+						slow_format_filetypes[vim.bo[bufnr].filetype] = true
+					end
+				end
+
+				return { timeout_ms = 200, lsp_fallback = true }, on_format
 			end,
 			format_after_save = function(bufnr)
 				-- only do async formatting when autoformatting is enabled, and sync formatting too slow
 				if vim.g.disable_autoformat or not slow_format_filetypes[vim.bo[bufnr].filetype] then
 					return
 				end
-				return { timeout_ms = 200, lsp_fallback = true }
+				return { lsp_fallback = true }
 			end,
 		})
 		vim.g.disable_autoformat = false
