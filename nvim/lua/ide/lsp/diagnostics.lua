9bc0f651ed4699f6b3a96cfff379491d99622423
Theodor Steiner <theosteiner@gmail.com>
2024-04-17 23:21:23 +0900

    chore: re-add legacy signs for plugins

--- nvim/lua/ide/lsp/diagnostics.lua
+++ nvim/lua/ide/lsp/diagnostics.lua
@@ -4,6 +4,11 @@
 local M = {}
 
 M.setup = function()
+	-- TODO: remove this legacy way of defining diagnostic signs once all plugins support it
+	vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
+	vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
+	vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
+	vim.fn.sign_define("DiagnosticSignHint", { text = "󰌵", texthl = "DiagnosticSignHint" })
 	-- make diagnostics show in float
 	vim.diagnostic.config({
 		virtual_text = false,
