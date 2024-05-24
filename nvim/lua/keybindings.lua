18fb409e547027e96de1000f3ffe1a12ef01feb5
Theodor Steiner <theosteiner@gmail.com>
2024-05-24 12:07:14 +0900

    feat: add ability to restart nvim

--- nvim/lua/keybindings.lua
+++ nvim/lua/keybindings.lua
@@ -2,8 +2,11 @@
 -- ************** KEYBINDINGS *************
 -- ****************************************
 
--- source vim config with <leader> enter
-vim.keymap.set("n", "<leader><CR>", "<cmd>lua Reload()<cr>")
+-- restart nvim with <leader> enter
+-- How does this work? This basically crashes the current nvim process (exit code 1 instead of 0)
+-- if nvim was started with the `.config/bin/nvim_auto_restart` script,
+-- a crash exit (code 1) will trigger a restart of nvim, while a normal exit (code 0) breaks the while loop
+vim.keymap.set("n", "<leader><CR>", "<cmd>lua os.exit(1)<cr>")
 
 -- delete highlights on escape
 vim.keymap.set("n", "<ESC>", ":noh<CR>", { silent = true })
