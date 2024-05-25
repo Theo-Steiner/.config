fc7c1f56189fce71064acbac97ddbdebd10464e7
Theodor Steiner <theosteiner@gmail.com>
2024-05-25 15:20:23 +0900

    feat: change to searchable neotree popup

--- nvim/lua/ui/_neotree.lua
+++ nvim/lua/ui/_neotree.lua
@@ -9,18 +9,39 @@ return {
 		-- open file tree witvh <leader> p
 		{ "<leader>pv", ":Neotree reveal toggle<cr>", silent = true },
 	},
+	cmd = "Neotree",
 	branch = "v3.x",
 	config = function()
 		require("neo-tree").setup({
+			event_handlers = {
+				{
+					event = "neo_tree_buffer_leave",
+					handler = function()
+						local state = require("neo-tree.sources.manager").get_state("filesystem")
+						local fs = require("neo-tree.sources.filesystem")
+						fs.reset_search(state, false)
+					end
+				}
+			},
 			close_if_last_window = true,
 			popup_border_style = "rounded",
 			window = {
-				width = 35,
+				position = "float",
 				mappings = {
-					["/"] = "noop",
+					["/"] = function(state)
+						if state.search_pattern ~= nil then
+							require("neo-tree.sources.filesystem.commands").clear_filter(state)
+						end
+						require("neo-tree.sources.filesystem.commands").filter_as_you_type(state)
+					end,
 				},
 			},
 			git_status_async = true,
+			default_component_configs = {
+				file_size = {
+					enabled = false,
+				},
+			},
 			filesystem = {
 				filtered_items = {
 					hide_by_name = {
