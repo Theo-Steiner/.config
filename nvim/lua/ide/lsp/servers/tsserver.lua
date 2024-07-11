7b0ac0698f6b4b7f67bb597419450467d78bcc24
Theodor Steiner <theosteiner@gmail.com>
2024-07-11 23:47:16 +0900

    chore: svelte or vue... why not both?

--- nvim/lua/ide/lsp/servers/tsserver.lua
+++ nvim/lua/ide/lsp/servers/tsserver.lua
@@ -7,14 +7,18 @@ return {
 						'/mason/packages/vue-language-server/node_modules/@vue/language-server/node_modules/@vue/typescript-plugin',
 				languages = { 'typescript', 'javascript', 'vue' }
 			},
-			-- TODO: as of today (4/16/2024) @vue/typescript-plugin does not work with:
-			-- "typescript-svelte-plugin",
-			-- it should however be fixed in this PR: https://github.com/sveltejs/language-tools/pull/2317
+			{
+				name = "typescript-svelte-plugin",
+				location = vim.fn.stdpath('data') ..
+						'/mason/packages/svelte-language-server/node_modules/typescript-svelte-plugin',
+				languages = { 'typescript', 'javascript', 'svelte' }
+			},
 		}
 	},
 	filetypes = {
 		"javascript",
 		"typescript",
+		"svelte",
 		"vue",
 	},
 }
