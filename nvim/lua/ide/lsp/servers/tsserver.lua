60b0ded769eef72ff8e128e776fbb5435c27e9df
Theodor Steiner <theosteiner@gmail.com>
2024-07-14 11:58:31 +0900

    fix: remove svelte from ts filetypes

--- nvim/lua/ide/lsp/servers/tsserver.lua
+++ nvim/lua/ide/lsp/servers/tsserver.lua
@@ -3,22 +3,21 @@ return {
 		plugins = {
 			{
 				name = "@vue/typescript-plugin",
-				location = vim.fn.stdpath('data') ..
-						'/mason/packages/vue-language-server/node_modules/@vue/language-server/node_modules/@vue/typescript-plugin',
-				languages = { 'typescript', 'javascript', 'vue' }
+				location = vim.fn.stdpath("data")
+					.. "/mason/packages/vue-language-server/node_modules/@vue/language-server/node_modules/@vue/typescript-plugin",
+				languages = { "typescript", "javascript", "vue" },
 			},
 			{
 				name = "typescript-svelte-plugin",
-				location = vim.fn.stdpath('data') ..
-						'/mason/packages/svelte-language-server/node_modules/typescript-svelte-plugin',
-				languages = { 'typescript', 'javascript', 'svelte' }
+				location = vim.fn.stdpath("data")
+					.. "/mason/packages/svelte-language-server/node_modules/typescript-svelte-plugin",
+				languages = { "typescript", "javascript", "svelte" },
 			},
-		}
+		},
 	},
 	filetypes = {
 		"javascript",
 		"typescript",
-		"svelte",
 		"vue",
 	},
 }
