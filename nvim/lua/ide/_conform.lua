485565767e984355f0551d1d14515fd8614091af
Theodor Steiner <theosteiner@gmail.com>
2024-08-07 15:13:21 +0900

    fix: formatter not falling back to lsp

--- nvim/lua/ide/_conform.lua
+++ nvim/lua/ide/_conform.lua
@@ -5,7 +5,6 @@ return {
 		local slow_format_filetypes = {}
 		require("conform").setup({
 			formatters_by_ft = {
-				lua = { "stylua" },
 				javascript = { "prettier" },
 				typescript = { "prettier" },
 				vue = { "prettier" },
@@ -30,14 +29,14 @@ return {
 					end
 				end
 
-				return { timeout_ms = 200, lsp_fallback = "last" }, on_format
+				return { timeout_ms = 200, lsp_format = "last" }, on_format
 			end,
 			format_after_save = function(bufnr)
 				-- only do async formatting when autoformatting is enabled, and sync formatting too slow
 				if vim.g.disable_autoformat or not slow_format_filetypes[vim.bo[bufnr].filetype] then
 					return
 				end
-				return { lsp_fallback = "last" }
+				return { lsp_format = "last" }
 			end,
 		})
 		vim.g.disable_autoformat = false
