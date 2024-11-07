dd4309930d56f85a5dc0b5c5774e4e797c5c7b40
Theodor Steiner <theosteiner@gmail.com>
2024-11-07 11:56:54 +0900

    chore: remove unnneeded noice settings

--- nvim/lua/ui/_noice.lua
+++ nvim/lua/ui/_noice.lua
@@ -11,42 +11,12 @@ return {
 		require("noice").setup({
 			presets = { long_message_to_split = true, },
 			lsp = {
-				progress = { enabled = false },
+				progress = { enabled = true },
 				override = {
 					["vim.lsp.util.convert_input_to_markdown_lines"] = false,
 					["vim.lsp.util.stylize_markdown"] = false,
 					["cmp.entry.get_documentation"] = false,
 				},
-			},
-			views = {
-				hover = {
-					relative = "cursor",
-				},
-				documentation = {
-					view = "mini"
-				},
-				mini = {
-					border = {
-						style = "rounded",
-					},
-					position = {
-						row = -2,
-					},
-					win_options = {
-						winblend = 0,
-						winhighlight = {
-							Normal = "NoiceCmdlinePopup",
-							FloatBorder = "NoiceCmdlinePopupBorder",
-						}
-					}
-				},
-				split = {
-					win_options = {
-						winhighlight = {
-							Normal = "BG_HIGHLIGHT"
-						},
-					}
-				}
 			},
 		})
 	end,
