a4cea87f59e1582e63a035865a1d9e24f20bb201
Theodor Steiner <theosteiner@gmail.com>
2024-11-07 11:53:15 +0900

    feat: disable auto format for big files

--- nvim/lua/tools/_snacks.lua
+++ nvim/lua/tools/_snacks.lua
@@ -1,4 +1,15 @@
 return {
 	"folke/snacks.nvim",
 	priority = 1000,
+	opts = {
+		bigfile = {
+			enable = true,
+			setup = function(ctx)
+				vim.g.disable_autoformat = true
+				vim.schedule(function()
+					vim.bo[ctx.buf].syntax = ctx.ft
+				end)
+			end
+		}
+	}
 }
