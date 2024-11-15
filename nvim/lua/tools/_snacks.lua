986b7a91ef03df047942266595cda68ddf518ba2
Theodor Steiner <theosteiner@gmail.com>
2024-11-15 19:47:15 +0900

    chore: upgrade plugins

--- nvim/lua/tools/_snacks.lua
+++ nvim/lua/tools/_snacks.lua
@@ -2,6 +2,7 @@ return {
 	"folke/snacks.nvim",
 	priority = 1000,
 	opts = {
+		-- files over 1.5MB will be loaded with special settings to improve startup time
 		bigfile = {
 			enable = true,
 			setup = function(ctx)
@@ -10,6 +11,12 @@ return {
 					vim.bo[ctx.buf].syntax = ctx.ft
 				end)
 			end
-		}
+		},
+		-- prettier notifications
+		notifier = { enabled = true },
+		-- skip loading plugins when writing to a not yet existing file
+		quickfile = { enabled = true },
+		-- auto show lsp references
+		words = { enabled = true },
 	}
 }
