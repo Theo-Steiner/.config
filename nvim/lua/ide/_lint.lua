1d04432f9b6a95ae26b5b2602759439da71bead3
Theodor Steiner <theodor.steiner@lycorp.co.jp>
2025-07-28 11:11:44 +0900

    chore: use eslint_d for linting

--- nvim/lua/ide/_lint.lua
+++ nvim/lua/ide/_lint.lua
@@ -2,22 +2,23 @@ local lint_with_root_dir = function()
 	local lint = require("lint")
 	local get_clients = vim.lsp.get_clients
 	local client = get_clients({ bufnr = 0 })[1] or {}
-	lint.try_lint(nil, { cwd = client.root_dir })
+	lint.try_lint(nil, { cwd = client.root_dir, ignore_errors = true })
 end
 
 return {
 	"mfussenegger/nvim-lint",
 	config = function()
 		local lint = require("lint")
+		-- use 'eslint_d' instead of 'eslint' to avoid node startup cost
+		local eslint = 'eslint_d'
 		-- attach linters for filetypes
 		lint.linters_by_ft = {
-			css = { "stylelint" },
-			javascript = { "eslint" },
-			javascriptreact = { "eslint" },
-			typescript = { "eslint" },
-			typescriptreact = { "eslint" },
-			vue = { "eslint" },
-			svelte = { "eslint" },
+			javascript = { eslint },
+			javascriptreact = { eslint },
+			typescript = { eslint },
+			typescriptreact = { eslint },
+			vue = { eslint },
+			svelte = { eslint },
 			go = { "golangcilint" }
 		}
 		-- setup lua autocommand to lint when first opening, after inserting and when writing the buffer.
